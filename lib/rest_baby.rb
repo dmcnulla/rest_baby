0600b584223e29da40f71193c3954552f109e9c6
Dave McNulla <david.mcnulla@resmed.com>
2014-02-13 21:59:16 -0800

    change comments stype to markdown.

--- lib/rest_baby.rb
+++ lib/rest_baby.rb
@@ -1,29 +1,24 @@
-=begin rdoc
-
-== Authors
-
-Dave McNulla
-
-=end
+# rest_baby.rb
+# @author Dave McNulla of ResMed
+# Originated: 2011
+# This library was written to simplify communications with rest services.
 
 require 'net/http'
 require 'net/https'
 require "rest_baby/version"
 
-##
 # RestBaby is a small rest client. It encapsulates some of the details for creating and using the rest service.
 module RestBaby
 	# Sends and receives data to a restful web service
 	class Client
 
-		##
 		# Creates a new rest client
 		#
-		# * +url+ = url of the rest service
+		# @param url [String] url of the rest service
 		# eg. http://myrestservice.com:80/time
-		# * +headers+ = default headers to use.
+		# @param headers [Hash] default headers to use.
 		# eg. '{ \"Content-Type\" => \"application/json\"}'
-		# can be multiple headers
+		# Can be multiple headers separated by commas inside the brackets.
 		def initialize(url, headers = {})
 			@uri = URI.parse(url)
 			@headers = headers
@@ -31,15 +26,13 @@ module RestBaby
 			@password = nil
 		end
 
-		##
 		# Modifies the headers by merging new headers with current headers. 
 		#
-		# * +headers+ = new headers to merge with current headers
+		# @param headers [Hash] new headers to merge with current headers
 		def set_headers(headers)
 			@headers = @headers.merge(headers)
 		end
 
-		##
 		# Modifies the authentication for the rest client
 		#
 		# * +user+ = authorized username
@@ -49,56 +42,51 @@ module RestBaby
 			@uri = URI.parse(new_url)
 		end
 
-		##
 		# Basic web services Get command
 		#
-		# * +url+ = url to send the command to
-		# * +headers+ = header parameters including authorization and Content-Type
-		# The response from the rest server is returned
+		# @param  path [String] Path of service to send the command to
+		# @param headers [String] header parameters including authorization and Content-Type
+		# @return The response from the rest server is returned
 		def get(headers = {})
 			return request(@uri, Net::HTTP::Get.new(@uri.request_uri), headers)
 		end
 
-		## 
 		# Basic web services Post command
 		# 
-		# * +url+ = url to send the command to
-		# * +body+ = data to put in the body
-		# * +headers+ = header parameters including authorization and Content-Type
-		# The response from the rest server is returned
+		# @param path [String] Path of service to send the command to
+		# @param body [Any type of string] Data to put in the body such as json or xml
+		# @param headers [Hash] header parameters including authorization and Content-Type
+		# @return The response from the rest server is returned
 		def post(body = nil, headers = {})
 			return request(@uri, Net::HTTP::Post.new(@uri.request_uri), body, headers)
 		end
 
-		## 
 		# Basic web services Delete command
 		# 
-		# * +url+ = url to send the command to
-		# * +headers+ = header parameters including authorization and Content-Type
-		# The response from the rest server is returned
+		# @param path [String] Path of service to send the command to
+		# @param headers [Hash] header parameters including authorization and Content-Type
+		# @return The response from the rest server is returned
 		def delete(headers = {})
 			return request(@uri, Net::HTTP::Delete.new(@uri.request_uri), headers)
 		end
-
-		## 
+ 
 		# Basic web services Put command
 		# 
-		# * +url+ = url to send the command to
-		# * +body+ = data to put in the body
-		# * +headers+ = header parameters including authorization and Content-Type
-		# The response from the rest server is returned
+		# @param path [String] Path of service to send the command to
+		# @param body [String] data to put in the body
+		# @param headers [Hash] header parameters including authorization and Content-Type
+		# @return Response from the rest server is returned
 		def put(body = nil, headers = {})
 			return request(@uri, Net::HTTP::Put.new(@uri.request_uri), body, headers)
 		end
 
-		## 
 		# Sending the web services command
 		#
-		# * +url+ = url to send the command to
-		# * +req+ = command to send as Net::HTTP:<command> class
-		# * +body+ = data to put in the body
-		# * +headers+ = header parameters including authorization and Content-Type
-		# The response from the rest server is returned
+		# @param uri [URI] Uri to send the command to
+		# @param request [HTTP Request] command to send as Net::HTTP:<command> class; get, post, put, delete.
+		# @param body [String] data to put in the body
+		# @param headers [Hash] header parameters including authorization and Content-Type
+		# @return The response from the rest server is returned
 		def request(uri, request, body = nil, headers)
 			@headers.merge(headers).each { |key, value| request[key] = value }
 			request.body = body unless body.nil?
@@ -117,10 +105,7 @@ module RestBaby
 			@wsresponse.code
 		end
 
-		## 
 		# Pretty print the web services last response
-		#
-		# The nice looking output is returned as a string
 		def print_last_response
 			output = "CODE = #{@wsresponse.code}\n"
 			output << "MESSAGE = #{@wsresponse.message}\n"
